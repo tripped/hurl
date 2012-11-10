0104031f46a2ddbb6240206bb2b2f09482d1d7c8
Trip Volpe <trip@fugazo.com>
2012-11-09 20:05:43 -0800

    Factor most cURL ops into an RAII wrapper

--- src/hurl.cpp
+++ src/hurl.cpp
@@ -13,6 +13,55 @@ namespace hurl
 {
     namespace detail
     {
+        class handle
+        {
+        public:
+            handle()
+                : handle_(curl_easy_init())
+            {
+                if (handle_ == NULL)
+                    throw std::runtime_error("curl_easy_init failed");
+            }
+
+            ~handle()
+            {
+                curl_easy_cleanup(handle_);
+            }
+
+            void perform()
+            {
+                if (CURLE_OK != curl_easy_perform(handle_))
+                    throw std::runtime_error("curl_easy_perform failed");
+            }
+
+            void reset()
+            {
+                curl_easy_reset(handle_);
+            }
+
+            template<typename T, typename U>
+            void setopt(T option, U value)
+            {
+                if (CURLE_OK != curl_easy_setopt(handle_, option, value))
+                    throw std::runtime_error("curl_easy_setopt failed");
+            }
+
+            template<typename T, typename U>
+            void getinfo(T info, U* ret)
+            {
+                if (CURLE_OK != curl_easy_getinfo(handle_, info, ret))
+                    throw std::runtime_error("curl_easy_getinfo failed");
+            }
+
+            CURL* get() const
+            {
+                return handle_;
+            }
+
+        private:
+            CURL* handle_;
+        };
+
         size_t writefunc(void* ptr, size_t size, size_t nmemb, httpresponse* resp)
         {
             resp->body.append(static_cast<char*>(ptr), size * nmemb);
@@ -39,95 +88,77 @@ namespace hurl
         }
 
 
-        void prepare_basic(CURL*                curl,
+        void prepare_basic(handle&              curl,
                            httpresponse &       response,
                            std::string const&   url)
         {
-            curl_easy_reset(curl);
-            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1);
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writefunc);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+            curl.reset();
+            curl.setopt(CURLOPT_URL, url.c_str());
+            curl.setopt(CURLOPT_NOPROGRESS, 1);
+            curl.setopt(CURLOPT_WRITEFUNCTION, &writefunc);
+            curl.setopt(CURLOPT_WRITEDATA, &response);
         }
 
-        void prepare_post(CURL*                 curl,
+        void prepare_post(handle&               curl,
                           const void*           data,
                           size_t                size)
         {
-            curl_easy_setopt(curl, CURLOPT_POST, 1);
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, size);
+            curl.setopt(CURLOPT_POST, 1);
+            curl.setopt(CURLOPT_POSTFIELDS, data);
+            curl.setopt(CURLOPT_POSTFIELDSIZE, size);
         }
 
 
-        httpresponse get(CURL*                  curl,
+        httpresponse get(handle&                curl,
                          std::string const&     url)
         {
             httpresponse result;
             prepare_basic(curl, result, url);
-
-            if (CURLE_OK != curl_easy_perform(curl))
-                throw std::runtime_error("curl_easy_perform failed");
-
-            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &result.status);
-
+            curl.perform();
+            curl.getinfo(CURLINFO_RESPONSE_CODE, &result.status);
             return result;
         }
 
-        httpresponse post(CURL*                 curl,
+        httpresponse post(handle&               curl,
                           std::string const&    url,
                           std::string const&    data)
         {
             httpresponse result;
             prepare_basic(curl, result, url);
             prepare_post(curl, data.data(), data.size());
-
-            if (CURLE_OK != curl_easy_perform(curl))
-                throw std::runtime_error("curl_easy_perform failed");
-
-            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &result.status);
-
+            curl.perform();
+            curl.getinfo(CURLINFO_RESPONSE_CODE, &result.status);
             return result;
         }
     }
 
     httpresponse get(std::string const& url)
     {
-        // TODO: these aren't exception-safe!
-        CURL* curl = curl_easy_init();
-        httpresponse result = detail::get(curl, url);
-        curl_easy_cleanup(curl);
-        return result;
+        detail::handle curl;
+        return detail::get(curl, url);
     }
 
     httpresponse get(std::string const& url, httpparams const& params)
     {
-        CURL* curl = curl_easy_init();
+        detail::handle curl;
 
         std::string query(url);
         if (!params.empty())
             query.append("?" + detail::serialize(params));
 
-        httpresponse result = detail::get(curl, query);
-
-        curl_easy_cleanup(curl);
-        return result;
+        return detail::get(curl, query);
     }
 
     httpresponse post(std::string const& url, std::string const& data)
     {
-        CURL* curl = curl_easy_init();
-        httpresponse result = detail::post(curl, url, data);
-        curl_easy_cleanup(curl);
-        return result;
+        detail::handle curl;
+        return detail::post(curl, url, data);
     }
 
     httpresponse post(std::string const& url, httpparams const& params)
     {
-        CURL* curl = curl_easy_init();
-        httpresponse result = detail::post(curl, url, detail::serialize(params));
-        curl_easy_cleanup(curl);
-        return result;
+        detail::handle curl;
+        return detail::post(curl, url, detail::serialize(params));
     }
 }
 
